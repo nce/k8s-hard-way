a7500de61a20f1044d73f4e3307081375f69a73f
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-02-22 13:40:17 +0100

    apiserver refactoring

--- infra/k8s_controller.tf
+++ infra/k8s_controller.tf
@@ -52,3 +52,51 @@ resource "null_resource" "k8s_instance_controller" {
     ]
   }
 }
+
+resource "local_file" "k8s_apiserver" {
+  count = var.controller_instances
+
+  content = templatefile("apiserver/apiserver.sh.tftpl", {
+    k8s_version          = var.k8s_version
+    controller_instances = var.controller_instances
+    etcd_server          = "https://${join(":2379,https://", aws_instance.controller.*.private_ip)}:2379"
+    cluster_private_ip   = aws_instance.bastion.private_ip
+    cluster_service_ip   = var.cluster_service_ip
+    encryption_key       = random_id.etcd_encryption_key.b64_std
+  })
+
+  filename = "./apiserver/generated/controller${count.index}.apiserver.sh"
+
+  depends_on = [aws_instance.controller]
+}
+
+resource "null_resource" "k8s_instance_controller_apiserver" {
+  count = var.controller_instances
+
+  depends_on = [
+    null_resource.k8s_ca,
+    null_resource.k8s_apiserver,
+    null_resource.k8s_service_account,
+    local_file.k8s_apiserver,
+  ]
+
+  connection {
+    type         = "ssh"
+    user         = "ec2-user"
+    host         = aws_instance.controller.*.private_ip[count.index]
+    bastion_host = aws_instance.bastion.public_ip
+  }
+
+  provisioner "file" {
+    source      = "./apiserver/generated/controller${count.index}.apiserver.sh"
+    destination = "apiserver.sh"
+  }
+
+  provisioner "remote-exec" {
+
+    inline = [
+      "sudo chmod +x apiserver.sh",
+      "sudo ./apiserver.sh"
+    ]
+  }
+}
