b7fd3a311d9188cfb03c05fa0869dc39d9564853
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-03-07 17:57:52 +0100

    typo

--- infra/k8s_worker.tf
+++ infra/k8s_worker.tf
@@ -1,12 +1,42 @@
 resource "local_file" "k8s_worker_cri" {
   content = templatefile("cri/crio.sh.tftpl", {
-    k8s_version = var.k8s_version
+    crio_version = var.crio_version
   })
 
   filename = "./crio/generated/crio.sh"
 
   depends_on = [aws_instance.worker]
 }
+
+resource "null_resource" "k8s_worker_baseos" {
+  count = var.worker_instances
+
+  depends_on = [
+    aws_instance.worker
+  ]
+
+  connection {
+    type         = "ssh"
+    user         = "ec2-user"
+    host         = aws_instance.worker.*.private_ip[count.index]
+    bastion_host = aws_instance.bastion.public_ip
+
+  }
+
+  provisioner "file" {
+    source      = "./baseos/baseos.sh"
+    destination = "baseos.sh"
+  }
+
+  provisioner "remote-exec" {
+
+    inline = [
+      "sudo chmod +x baseos.sh",
+      "sudo ./baseos.sh"
+    ]
+  }
+}
+
 resource "null_resource" "k8s_instance_worker_cri" {
   count = var.worker_instances
 
@@ -47,8 +77,9 @@ resource "local_file" "k8s_worker_kubelet" {
   filename = "./kubelet/generated/worker${count.index}-kubelet.sh"
 
   depends_on = [
+    aws_instance.worker,
     null_resource.k8s_instance_worker_cri,
-    aws_instance.worker
+    null_resource.k8s_controller_baseos
   ]
 }
 
@@ -95,8 +126,7 @@ resource "local_file" "k8s_worker_proxy" {
   filename = "./kube-proxy/generated/worker${count.index}-kube-proxy.sh"
 
   depends_on = [
-    aws_instance.worker,
-    null_resource.k8s_instance_worker_cri,
+    aws_instance.worker
   ]
 }
 
@@ -104,6 +134,7 @@ resource "null_resource" "k8s_instance_worker_proxy" {
   count = var.worker_instances
 
   depends_on = [
+    null_resource.k8s_instance_worker,
     null_resource.k8s_ca_worker,
     null_resource.k8s_proxy,
     null_resource.k8s_admin,
