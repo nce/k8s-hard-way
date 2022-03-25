94e85757f3b9cf7cc127346ec9b1a35d23569206
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-03-25 17:16:06 +0100

    AWS LB/cloud-controller permissions

--- infra/aws_iam.tf
+++ infra/aws_iam.tf
@@ -8,7 +8,7 @@ resource "aws_iam_access_key" "ingress_lb" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name   = "iam_aws_lb"
+  name   = "AWSLoadBalancerControllerIAMPolicy"
   policy = file("aws-lb-controller/iam_policy.json")
 }
 
@@ -23,22 +23,256 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
   policy_arn = aws_iam_policy.policy.arn
 }
 
-resource "kubectl_manifest" "aws_iam_token" {
+#resource "aws_iam_role_policy_attachment" "test-attach" {
+#  role       = aws_iam_role.role.name
+#  policy_arn = aws_iam_policy.policy.arn
+#}
+#
+#resource "aws_iam_role" "ingress_lb" {
+#  name = "test-role"
+#
+#  assume_role_policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": "ec2.amazonaws.com"
+#      },
+#      "Effect": "Allow",
+#      "Sid": ""
+#    }
+#  ]
+#}
+#EOF
+#}
 
-  depends_on = [
-    time_sleep.wait_for_k8s_api
-  ]
+#resource "kubectl_manifest" "aws_iam_token" {
+#
+#  depends_on = [
+#    time_sleep.wait_for_k8s_api
+#  ]
+#
+#  yaml_body = <<YAML
+#apiVersion: v1
+#data:
+#  keyid: ${base64encode(aws_iam_access_key.ingress_lb.id)}
+#  keysecret: ${base64encode(aws_iam_access_key.ingress_lb.secret)}
+#kind: Secret
+#metadata:
+#  name: awscredentials
+#  namespace: kube-system
+#type: Opaque
+#YAML
+#}
 
-  yaml_body = <<YAML
-apiVersion: v1
-data:
-  keyid: ${base64encode(aws_iam_access_key.ingress_lb.id)}
-  keysecret: ${base64encode(aws_iam_access_key.ingress_lb.secret)}
-kind: Secret
-metadata:
-  name: awscredentials
-  namespace: kube-system
-type: Opaque
-YAML
+
+resource "aws_iam_instance_profile" "aws_cloud_provider_controller" {
+  name = "aws_cloud_provider_controller"
+  role = aws_iam_role.aws_cloud_provider_controller.name
 }
 
+resource "aws_iam_role" "aws_cloud_provider_controller" {
+  name = "aws_cloud_provider_controller"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "aws_cloud_provider_controller" {
+  name = "ControlPlanePolicy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifyVolume",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateRoute",
+        "ec2:DeleteRoute",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:AttachLoadBalancerToSubnets",
+        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancerPolicy",
+        "elasticloadbalancing:CreateLoadBalancerListeners",
+        "elasticloadbalancing:ConfigureHealthCheck",
+        "elasticloadbalancing:DeleteLoadBalancer",
+        "elasticloadbalancing:DeleteLoadBalancerListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeLoadBalancerAttributes",
+        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:ModifyLoadBalancerAttributes",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:CreateListener",
+        "elasticloadbalancing:CreateTargetGroup",
+        "elasticloadbalancing:DeleteListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancerPolicies",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:ModifyTargetGroup",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:DeregisterTargets",
+        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+        "iam:CreateServiceLinkedRole",
+        "kms:DescribeKey"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "aws_cloud_provider_controller" {
+  role       = aws_iam_role.aws_cloud_provider_controller.name
+  policy_arn = aws_iam_policy.aws_cloud_provider_controller.arn
+}
+
+
+
+
+
+
+
+resource "aws_iam_instance_profile" "aws_cloud_provider_worker" {
+  name = "aws_cloud_provider_worker"
+  role = aws_iam_role.aws_cloud_provider_worker.name
+}
+
+resource "aws_iam_role" "aws_cloud_provider_worker" {
+  name = "aws_cloud_provider_worker"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+resource "aws_iam_policy" "aws_cloud_provider_worker" {
+  name = "NodePolicy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:BatchGetImage"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "aws_cloud_provider_worker" {
+  role       = aws_iam_role.aws_cloud_provider_worker.name
+  policy_arn = aws_iam_policy.aws_cloud_provider_worker.arn
+}
+
+resource "aws_iam_role_policy_attachment" "aws_external_dns" {
+  role       = aws_iam_role.aws_cloud_provider_worker.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
+resource "aws_iam_role_policy_attachment" "aws_external_dns_controller" {
+  role       = aws_iam_role.aws_cloud_provider_controller.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
+
+resource "aws_iam_policy" "external_dns" {
+  name = "externalDns"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets"
+      ],
+      "Resource": [
+        "arn:aws:route53:::hostedzone/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
