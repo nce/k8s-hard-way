1712f1cf54c1cfff39fd8f81a3e39681124d8e8f
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-04-08 18:18:41 +0200

    init ignition module

---
+++ terraform/cluster-infra/modules/ignition/s3.tf
@@ -0,0 +1,51 @@
+resource "aws_s3_bucket" "ignition" {
+  bucket = "${var.k8s_cluster_name}-ignition-configs"
+}
+
+resource "aws_s3_bucket_acl" "ignition" {
+  bucket = aws_s3_bucket.ignition.bucket
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "ignition" {
+  bucket = aws_s3_bucket.ignition.id
+
+  restrict_public_buckets = true
+
+  block_public_acls   = true
+  block_public_policy = true
+
+  ignore_public_acls = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ignition" {
+  bucket = aws_s3_bucket.ignition.id
+
+  rule {
+    bucket_key_enabled = false
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_iam_policy" "ignition_s3" {
+  name   = "${var.k8s_cluster_name}-s3"
+  path   = "/${var.k8s_cluster_name}/"
+  policy = data.aws_iam_policy_document.ignition.json
+}
+
+data "aws_iam_policy_document" "ignition" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.ignition.arn,
+      "${aws_s3_bucket.ignition.arn}/*",
+    ]
+  }
+}
---
+++ terraform/cluster-infra/modules/ignition/variables.tf
@@ -0,0 +1,7 @@
+variable "k8s_cluster_name" {
+  type = string
+}
+
+variable "files" {
+  type = map(object({ content = string, user = string, group = string, mode = string }))
+}
