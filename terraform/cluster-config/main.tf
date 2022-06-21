314ee93f110d57451c6dae91456598eefdf5dcc3
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-06-21 11:54:22 +0200

    Add a lot of (outdated) debug

--- terraform/cluster-config/main.tf
+++ terraform/cluster-config/main.tf
@@ -24,23 +24,49 @@ module "kube_proxy" {
 }
 
 # does not need a vpc if we run in host network
-# which might be a better solution anyway, as its a core component
-#module "aws_cloud_controller_manager" {
-#  source = "./modules/aws-cloud-controller-manager"
-#}
+## which might be a better solution anyway, as its a core component
+# Nodelabeler Systemd services needs to run for this
+# as the ds is selecting on this label for master node
+# CNI needs to be available; as there are no tolerations
+# for kubelet not ready, which is ready after the cni
+#
+# E0601 12:56:57.873916       1 node_controller.go:213] error syncing 'ip-10-10-148-251': failed to get provider ID for node ip-10-10-148-251 at cloudprovider: failed to get instance ID from cloud provider: instance not found, requeuing
+#
+# => RBN in the subnet
+module "aws_cloud_controller_manager" {
+  source = "./modules/aws-cloud-controller-manager"
 
+  chart_version    = var.aws_cloud_controller_manager_version
+  k8s_api_extern   = var.k8s_api_extern
+  k8s_cluster_name = var.k8s_cluster_name
+
+}
+
+# CNI not working with 
+#   Warning  FailedCreatePodSandBox  2m40s             kubelet            Failed to create pod sandbox: rpc error: code = Unknown desc = failed to setup network for sandbox "5a156b99f77d0d53d679b5baaefd2094df28d6d575a37334d33e85454935
+# abb2": plugin type="loopback" failed (add): incompatible CNI versions; config is "1.0.0", plugin supports ["0.1.0" "0.2.0" "0.3.0" "0.3.1" "0.4.0"]
+#
+# AWS VPC Subnet config not needed as its picked up correctly
+#
+# Found availability zone: eu-central-1b
+# Discovered the instance primary IPv4 address: 10.10.173.15
+# Found instance-type: t4g.small
+# Found subnet-id: subnet-0a5bb0c1c84b6d146
+# => kubelet external cloudprovider was not set
 module "aws-vpc-cni-k8s" {
   source = "./modules/aws-vpc-cni-k8s"
 
-  chart_version = var.aws_vpc_cni_k8s
+  chart_version    = var.aws_vpc_cni_k8s
+  k8s_cluster_name = var.k8s_cluster_name
 }
 
 # with the csr approver the apiserver can access the kubelet
-# after this we have 'ready' nodes
+# which is needed for logs & exec
+# after this we have really 'ready' nodes
 module "kubelet-csr-approver" {
   source = "./modules/kubelet-csr-approver"
 
   chart_version = var.kubelet_csr_approver_version
 }
-
-
+#
+##
